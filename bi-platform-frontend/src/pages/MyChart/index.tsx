7916407303aad846049c43e632f3163ea749a9a6
Huh-x <97139830+Huh-x@users.noreply.github.com>
2024-04-19 11:40:51 +0800

    fix：frontend 列表数据美化
    【1】引入搜索框
    【2】分页实现
    【3】数据处理（对响应数据进行处理）
    【4】列表数据展示调整

--- bi-platform-frontend/src/pages/MyChart/index.tsx
+++ bi-platform-frontend/src/pages/MyChart/index.tsx
@@ -1,12 +1,16 @@
 import { listChartByPageUsingPost } from '@/services/noob-bi/chartController';
 import { message,Avatar, List, Space } from 'antd';
+import {useModel} from '@@/exports';
 import EChartsReact from 'echarts-for-react';
 import React, { useEffect, useState } from 'react';
+import Search from 'antd/es/input/Search';
 
 const MyChart: React.FC = () => {
 
   // 构建初始条件（便于后面恢复初始条件）
   const initSearchParams = {
+    // 默认第一页
+    current: 1,
     // 初始情况每页数据返回12条
     pageSize:12,
   }
@@ -19,6 +23,12 @@ const MyChart: React.FC = () => {
    */
   const [searchParams,setSearchParams] = useState<API.ChartQueryRequest>({...initSearchParams});
 
+  // 从全局状态中获取登陆用户信息
+  const {initialState} = useModel('@@initialState');
+  const {currentUser} = initialState??{};
+  // 定义数据加载状态（控制页面是否加载：默认加载）
+  // const {loading,setLoading} = useState<boolean>(true);
+
   // 定义变量存储图表数据
   const [chartList,setChartList] = useState<API.Chart[]>();
   // 定义数据总数（类型为number、默认为0）
@@ -26,6 +36,9 @@ const MyChart: React.FC = () => {
 
   // 定义一个获取数据的异步函数
   const loadData = async()=>{
+    // 获取数据（此时页面还在加载中，设定加载属性）
+    // setLoading(true);
+
     /**
      * 调用后端接口，传入serchParams请求参数并返回响应结果
      * listChartByPageUsingPost是通过openAPI生成的接口
@@ -38,6 +51,19 @@ const MyChart: React.FC = () => {
           setChartList(res.data.records ?? []);
           // 数据总数：数据列表如果为空则返回0
           setTotal(res.data.total ?? 0);
+
+          // 列表数据处理（有些图表有标题、有些没有，此处过滤掉不展示标题信息）
+          if(res.data.records){
+            res.data.records.forEach(data => {
+              // 将后端返回的图表字符串修改为对象数组，如果后端返回空字符串则返回{}
+              const chartOption = JSON.parse(data.genChart??'{}');
+              // 标题设置为undefined
+              chartOption.title = undefined;
+              // 将修改后的option重新赋值给原genChart字段
+              data.genChart = JSON.stringify(chartOption);
+            });
+          }
+
         }else{
           // 如果后端返回数据为空，抛出异常（获取图表失败）
           message.error("获取图表失败");
@@ -46,6 +72,9 @@ const MyChart: React.FC = () => {
         // 出现异常，提示失败西悉尼
         message.error('图表获取失败'+e.message);
       }
+
+      // 数据获取成功，加载完毕，将加载状态设置为false
+      // setLoading(false);
   }
 
   // 页面首次加载，触发加载数据
@@ -57,23 +86,50 @@ const MyChart: React.FC = () => {
   return (
     // 页面信息定义（my-chart）
     <div className = "my-chart">
+      {/* 搜索框引入 */}
+      <div>
+        
+        {/* <Search placeholder='请输入图表名称' enterButton loading={loading} onSearch={(value)=>{ */}
+        <Search placeholder='请输入图表名称' enterButton onSearch={(value)=>{
+          // 设置搜索条件
+          setSearchParams({
+            // 原始搜索条件
+            ...initSearchParams,
+            // 搜索词
+            name:value,
+          });
+        }}></Search>
+      </div>
 
+
+      {/* 数据引入 */}
       <List
           itemLayout="vertical"
           size="large"
+
+          // 分页组件定义
           pagination={{
-            onChange: (page) => {
-              console.log(page);
+            // 当切换分页，在当前搜索条件的基础上，将页数调整为当前的页数
+            onChange: (page,pageSize) => {
+              setSearchParams({
+                ...searchParams,
+                current:page,
+                pageSize,
+              })
             },
-            pageSize: 3,
+            // 显示当前页数
+            current: searchParams.current,
+            // 页面参数、总数修改为自己的
+            pageSize: searchParams.pageSize,
+            total: total,
           }}
+
+          // loading状态调整为自己的状态
+          // loading = {loading};
+
           // 数据源改成图表数据（列表组件会自动渲染）
           dataSource={chartList}
-          footer={
-            <div>
-              <b>ant design</b> footer part
-            </div>
-          }
+          
           renderItem={(item) => (
             // List.Item 要展示的每一条数据
             <List.Item
@@ -91,7 +147,8 @@ const MyChart: React.FC = () => {
               {/* 要展示的图表元素信息 */}
               <List.Item.Meta
                 // 头像（todo）
-                avatar={<Avatar src={'https://cos.holic-x.com/profile/avatar/avatar02.png'} />}
+                // avatar={<Avatar src={'https://cos.holic-x.com/profile/avatar/avatar02.png'} />}
+                avatar={<Avatar src={currentUser&&currentUser.userAvatar} />}
                 // 图表名称
                 title={<a href={item.href}>{item.name}</a>}
                 // 描述
@@ -101,7 +158,8 @@ const MyChart: React.FC = () => {
               {'分析目标：'+item.goal}
 
               {/* 图表信息展示 */}
-              <EChartsReact option={JSON.parse(item.genChart??'{}')} />
+              {/* <EChartsReact option={JSON.parse(item.genChart??'{}')} /> */}
+              <EChartsReact option={item.genChart&&JSON.parse(item.genChart)} />
 
             </List.Item>
           )}
